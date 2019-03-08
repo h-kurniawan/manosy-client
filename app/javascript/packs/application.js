04e78f91c4fafd457a2cdcff1e734646708971c7
Piao <kpaku@mdsol.com>
2019-03-08 16:43:46 +0900

    Make it editable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,33 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+
+import { EditorState } from "prosemirror-state"
+import { EditorView } from "prosemirror-view"
+import { Schema, DOMParser } from "prosemirror-model"
+import { schema } from "prosemirror-schema-basic"
+import { addListNodes } from "prosemirror-schema-list"
+import { exampleSetup } from "prosemirror-example-setup"
+
+
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+document.addEventListener("DOMContentLoaded", () => {
+  // create a schema with list support.
+  const mySchema = new Schema({
+    nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+    marks: schema.spec.marks
+  })
+
+  const editables = document.querySelectorAll("[data-editable]")
+  for (let editable of editables) {
+    const editor = editable.nextElementSibling
+    editable.style.display = "none"
+
+    new EditorView(editor, {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema).parse(editable),
+        plugins: exampleSetup({ schema: mySchema })
+      })
+    })
+  }
+})
